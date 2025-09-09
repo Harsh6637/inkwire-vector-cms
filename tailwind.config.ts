537eb341af92a3e682b28be5a1c300baba07c535
Harsh <harshpatel8877@gmail.com>
2025-09-09 17:03:17 -0600

    fixing CSS on Vercel

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,11 +4,11 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   content: [
     './index.html',
-    './src/**/*.{ts,tsx}', // include all TypeScript and TSX files
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {
-      // add custom colors, fonts, spacing, etc. here if needed
+
     },
   },
   plugins: [],
